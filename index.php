cf9e181bb1866456e442202b092a9df22583ee31
illfamed <generalarmyhf@gmail.com>
2024-11-27 16:03:22 +0100

    part 5 small changes

--- index.php
+++ index.php
@@ -22,76 +22,6 @@
 <body class="bg-dark">
   <div class="container text-white p-5 m-5 rounded mx-auto">
     <div class="row">
-      <div class="col">
-        <?php if (!isset($_SESSION['loggedin'])) { ?>
-          <div class="card shadow-lg text-white mb-3 panel panel-default">
-            <h1 class="card-header text-center shadow-lg p-3 panel-heading"><i class="bi bi-person-fill"></i> Log In</h1>
-            <div class="card-body panel-body">
-              <?php if (isset($_SESSION['message'])) { ?>
-                <div class="row">
-                  <div id="loginError" class="alert alert-danger"><?php echo $_SESSION['message'] ?></div>
-                  <?php if (isset($_SESSION['message'])) {
-                    unset($_SESSION['message']);
-                  } ?>
-                </div>
-              <?php } ?>
-              <?php if (isset($_SESSION['success'])) { ?>
-                <div class="row">
-                  <div id="loginSuccess" class="alert alert-success"><?php echo $_SESSION['success'] ?></div>
-                  <?php if (isset($_SESSION['success'])) {
-                    unset($_SESSION['success']);
-                  } ?>
-                </div>
-              <?php } ?>
-              <form id="loginForm" class="p-5 w-50 mx-auto" action="database/login.php" method="POST">
-                <div class="row">
-                  <label for="email" class="text-start">Email</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="bi bi-envelope"></i></span>
-                    </div>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="user@email.com" required />
-                  </div>
-                  <label for="password" class="text-start mt-4">Wachtwoord</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="bi bi-key"></i></span>
-                    </div>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="wachtwoord" required />
-                  </div>
-                </div>
-                <div class="row p-3 mt-4">
-                  <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="registercheckbox" name="registercheckbox">
-                    <label class="form-check-label" for="registercheckbox">Registreren</label>
-                  </div>
-                </div>
-                <div class="row">
-                  <button type="submit" class="mt-5 btn btn-primary shadow-lg mx-auto">
-                    <i class="bi bi-box-arrow-in-right"></i> Log In
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        <?php } elseif (isset($_SESSION['loggedin'])) { ?>
-          Phemex server tijd: <?php getServerTime() ?>
-          <div class="card shadow-lg text-white mb-3 panel panel-default">
-            <h1 class="card-header text-center shadow-lg p-3 panel-heading">
-              <p class="float-start"><i class="bi bi-person-fill"></i> Welkom <?php echo $_SESSION['email'] ?></p>
-              <a href="database/logout.php" class="btn btn-primary d-inline float-end"><i class="bi bi-box-arrow-right"></i> Log Out</a>
-              <a href="addaccount.php" class="btn btn-success d-inline float-end mx-2"><i class="bi bi-plus-lg"></i> Account toevoegen</a>
-            </h1>
-            <div class="card-body panel-body p-5">
-              <div class="row">
-                <?php getClosedTrades() ?>
-              </div>
-            </div>
-          </div>
-        <?php } ?>
-      </div>
-    </div>
-    <div class="row">
       <div id="draggablePanelList" class="col connectedSortable">
         <div class="card shadow-lg text-white mb-3 panel panel-default">
           <h1 class="card-header text-center shadow-lg p-3 panel-heading"><i class="bi bi-currency-exchange"></i> RR</h1>
@@ -191,6 +121,76 @@
             </form>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <?php if (!isset($_SESSION['loggedin'])) { ?>
+          <div class="card shadow-lg text-white mb-3 panel panel-default">
+            <h1 class="card-header text-center shadow-lg p-3 panel-heading"><i class="bi bi-person-fill"></i> Log In</h1>
+            <div class="card-body panel-body">
+              <?php if (isset($_SESSION['message'])) { ?>
+                <div class="row">
+                  <div id="loginError" class="alert alert-danger"><?php echo $_SESSION['message'] ?></div>
+                  <?php if (isset($_SESSION['message'])) {
+                    unset($_SESSION['message']);
+                  } ?>
+                </div>
+              <?php } ?>
+              <?php if (isset($_SESSION['success'])) { ?>
+                <div class="row">
+                  <div id="loginSuccess" class="alert alert-success"><?php echo $_SESSION['success'] ?></div>
+                  <?php if (isset($_SESSION['success'])) {
+                    unset($_SESSION['success']);
+                  } ?>
+                </div>
+              <?php } ?>
+              <form id="loginForm" class="p-5 w-50 mx-auto" action="database/login.php" method="POST">
+                <div class="row">
+                  <label for="email" class="text-start">Email</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                    </div>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="user@email.com" required />
+                  </div>
+                  <label for="password" class="text-start mt-4">Wachtwoord</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="bi bi-key"></i></span>
+                    </div>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="wachtwoord" required />
+                  </div>
+                </div>
+                <div class="row p-3 mt-4">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="registercheckbox" name="registercheckbox">
+                    <label class="form-check-label" for="registercheckbox">Registreren</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <button type="submit" class="mt-5 btn btn-primary shadow-lg mx-auto">
+                    <i class="bi bi-box-arrow-in-right"></i> Log In
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        <?php } elseif (isset($_SESSION['loggedin'])) { ?>
+          Phemex server tijd: <?php getServerTime() ?>
+          <div class="card shadow-lg text-white mb-3 panel panel-default">
+            <h1 class="card-header text-center shadow-lg p-3 panel-heading">
+              <p class="float-start"><i class="bi bi-person-fill"></i> Welkom <?php echo $_SESSION['email'] ?></p>
+              <a href="database/logout.php" class="btn btn-primary d-inline float-end"><i class="bi bi-box-arrow-right"></i> Log Out</a>
+              <a href="addaccount.php" class="btn btn-success d-inline float-end mx-2"><i class="bi bi-plus-lg"></i> Account toevoegen</a>
+            </h1>
+            <div class="card-body panel-body p-5">
+              <div class="row">
+                <?php getClosedTrades() ?>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
       </div>
     </div>
   </div>
