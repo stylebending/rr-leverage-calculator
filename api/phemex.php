fe45eb8bdfba2786faaf4c67d61130484e2d5f4d
illfamed <generalarmyhf@gmail.com>
2024-11-30 19:39:43 +0100

    endpoint changes

--- api/phemex.php
+++ api/phemex.php
@@ -67,6 +67,7 @@ function getClosedPositions()
             $d = date("d", $now);
             $y = date("Y", $now);
             $expiry = mktime($h, $i, $s, $m, $d, $y);
+            // TODO: de m-2 hieronder hele jaar door werkend maken
             $start = mktime($h, $i, $s, $m - 2, $d, $y);
             $requestPath = "/exchange/order/v2/orderList";
             $queryString = "?currency=USDT&ordStatus=7&start=" . $start . "000" . "&end=" . $now . "000" . "&offset=0&limit=200";
