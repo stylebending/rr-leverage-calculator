e4b1df343ff79ac5d86fcc714398572bfe3710ce
illfamed <generalarmyhf@gmail.com>
2024-12-04 21:57:06 +0100

    inverse changes

--- api/phemex.php
+++ api/phemex.php
@@ -40,6 +40,76 @@ function getServerTime()
 
 // TODO: functie getAccounts maken om daarop getClosedTrades te callen in de view indien nodig
 
+function groupOrdersIntoTrades($orders)
+{
+  $trades = [];
+  $currentTrade = [];
+  $isInTrade = false;
+  // Reverse the orders array so that we start from the last order
+  $orders = array_reverse($orders);
+
+  foreach ($orders as $order) {
+    // Check for an entry order (closedPnlEv == 0)
+    if ($order['closedPnlEv'] == 0) {
+      // If we were already in a trade, finalize the current trade and start a new one
+      if ($isInTrade) {
+        $trades[] = $currentTrade;
+        $currentTrade = [];
+      }
+
+      // Start a new trade with the current entry order
+      $isInTrade = true;
+    }
+
+    // If we are in a trade, add the current order to the trade
+    if ($isInTrade) {
+      $currentTrade[] = $order;
+    }
+  }
+
+  // Add the last trade if it's still open (i.e., there was no subsequent entry order)
+  if ($isInTrade) {
+    $trades[] = $currentTrade;
+  }
+
+  return array_reverse($trades);
+}
+
+function groupInverseOrdersIntoTrades($orders)
+{
+  $trades = [];
+  $currentTrade = [];
+  $isInTrade = false;
+  // Reverse the orders array so that we start from the last order
+  $orders = array_reverse($orders);
+
+  foreach ($orders as $order) {
+    // Check for an entry order (closedPnlEv == 0)
+    if ($order['side'] == $order['posSide']) {
+      // If we were already in a trade, finalize the current trade and start a new one
+      if ($isInTrade) {
+        $trades[] = $currentTrade;
+        $currentTrade = [];
+      }
+
+      // Start a new trade with the current entry order
+      $isInTrade = true;
+    }
+
+    // If we are in a trade, add the current order to the trade
+    if ($isInTrade) {
+      $currentTrade[] = $order;
+    }
+  }
+
+  // Add the last trade if it's still open (i.e., there was no subsequent entry order)
+  if ($isInTrade) {
+    $trades[] = $currentTrade;
+  }
+
+  return array_reverse($trades);
+}
+
 function getClosedPositions()
 {
   $connection = connectDB();
@@ -88,49 +158,119 @@ function getClosedPositions()
             $responseData = json_decode($response, true);
             $tradeHistory = $responseData['data'];
 
-            foreach ($tradeHistory as $trade => $tradeData) {
+            $groupedOrders = groupInverseOrdersIntoTrades($tradeHistory);
+            // Voor elke trade
+            foreach ($groupedOrders as $trade) {
+              // Sort orders in the current trade by transaction time
+              usort($trade, function ($a, $b) {
+                return $a['updatedAt'] <=> $b['updatedAt']; // Compare transactTimeNs values
+              });
+              $laatsteOrder = array_key_last($trade);
+              $laatsteDatum = round($trade[$laatsteOrder]['updatedAt'] / 1000);
+              $lD = date("d-m-Y H:i:s", $laatsteDatum);
+              $slGrootte = "";
+              $slPrijs = "";
+              $tps = [];
+              // Voor elke order
+              foreach ($trade as $order) {
+                $symbol = $order['symbol'];
+                // Als order een entry is
+                if ($order['side'] == $order['posSide']) {
+                  $entryTransactTimeUnix = round($order['updatedAt'] / 1000);
+                  $entryTransactTime = date("d-m-Y H:i:s", $entryTransactTimeUnix);
+                  $positieGroottte = $order['execQtyRq'];
+                  if ($order['side'] == 1) {
+                    $side = "Long";
+                  } else if ($order['side'] == 2) {
+                    $side = "Short";
+                  }
+                  $entry = round($order['priceRp'], 4);
+                }
+                // Als order een sl is
+                if ($order['tradeType'] == 6) {
+                  $slGrootte = ($order['execQtyRq'] / $positieGroottte) * 100;
+                  $slPrijs = round($order['priceRp'], 4);
+                }
+                // Als order een tp is
+                if ($order['tradeType'] == 7) {
+                  $tpPrijs = round($order['priceRp'], 4);
+                  $tpGrootte = ($order['execQtyRq'] / $positieGroottte) * 100;
+                  $tps[$tpPrijs] = $tpGrootte;
+                }
+              }
+
+              $rr = 0;
+              $wp = 0;
+              // TODO: Deze berekening goed checken bij allemaal verschillende trade scenarios
+              // voor elk scenaria uitdenken, long, short, win, loss, no tps only sl, only sl no tps
+              if ($side == "Long") {
+                foreach ($tps as $tpP => $tpG) {
+                  $wp += ((($tpP - $entry) / $entry) * 100) * ($tpG / 100);
+                }
+                if (!empty($slPrijs)) {
+                  $slp = $slGrootte / 100;
+
+                  $slToSubstract = ((($slPrijs - $entry) / $entry) * 100) * $slp;
+                  $actualWp = $wp + $slToSubstract;
+
+                  // RR = WP% / SL%
+                  $rr = round($actualWp / ($slp * 100), 2);
+                } else {
+                  $rr = "Open trade";
+                }
+              } else if ($side == "Short") {
+                foreach ($tps as $tpP => $tpG) {
+                  $wp += ((($tpP - $entry) / $entry) * 100) * ($tpG / 100) * -1;
+                }
+                if (!empty($slPrijs)) {
+                  $slp = $slGrootte / 100;
+
+                  $slToSubstract = ((($slPrijs - $entry) / $entry) * 100) * $slp * -1;
+                  $actualWp = $wp + $slToSubstract;
+
+                  // RR = WP% / SL%
+                  $rr = round($actualWp / ($slp * 100), 2);
+                } else {
+                  $rr = "Open trade";
+                }
+              }
+
+              // TODO: voor nu 1 entry, 1 sl en meerdere tps, maar iets verzinnen voor trades met meerdere entrie en sls
+
+              echo '<div class="card tCard shadow-lg text-white mb-3">' .
+                '<div class="card-header shadow-lg"><div class="row">' . '<h5 class="col text-start">' . $side . '</h5><h5 class="col text-center">' . $symbol . '</h5><h5 class="col text-end">' . $entryTransactTime . '</h5></div>' .
+                '</div>' .
+                '<div class="card-body row mx-auto">' .
+                '<div class="border border-white text-center text-justify shadow-lg text-white rounded p-5 my-5">' .
+                '<div class="row">' .
+                '<div class="col border-white border-end">' .
+                "RR " . "<br>" .
+                "<hr>" .
+                "SL prijs " . "<br>" .
+                "<hr>" .
+                "Laatste order " . "<br>" .
+                "<hr>" .
+                '</div>' .
+                '<div class="col">' .
+                $rr . "<br>" .
+                "<hr>" .
+                $slPrijs . "<br>" .
+                "<hr>" .
+                $lD . "<br>" .
+                "<hr>" .
+                '</div>' .
+                '</div>' .
+                "</div>" .
+                "</div>" .
+                "</div>";
+              echo "<br>";
             }
-            echo "Komt binnenkort, inverse posities wel beschikbaar.";
           }
         }
       }
     }
 
 
-    function groupOrdersIntoTrades($orders)
-    {
-      $trades = [];
-      $currentTrade = [];
-      $isInTrade = false;
-      // Reverse the orders array so that we start from the last order
-      $orders = array_reverse($orders);
-
-      foreach ($orders as $order) {
-        // Check for an entry order (closedPnlEv == 0)
-        if ($order['closedPnlEv'] == 0) {
-          // If we were already in a trade, finalize the current trade and start a new one
-          if ($isInTrade) {
-            $trades[] = $currentTrade;
-            $currentTrade = [];
-          }
-
-          // Start a new trade with the current entry order
-          $isInTrade = true;
-        }
-
-        // If we are in a trade, add the current order to the trade
-        if ($isInTrade) {
-          $currentTrade[] = $order;
-        }
-      }
-
-      // Add the last trade if it's still open (i.e., there was no subsequent entry order)
-      if ($isInTrade) {
-        $trades[] = $currentTrade;
-      }
-
-      return array_reverse($trades);
-    }
 
 
     function getClosedInversePositions()
@@ -209,8 +349,6 @@ function getClosedPositions()
                     }
                     // Als order een sl is
                     if ($order['orderType'] == "Stop") {
-                      $slTransactTimeUnix = round($order['transactTimeNs'] / 1000000000);
-                      $slTransactTime = date("d-m-Y H:i:s", $slTransactTimeUnix);
                       $slGrootte = ($order['orderQty'] / $positieGroottte) * 100;
                       $slPrijs = round($order['priceEp'] / 10000, 2);
                     }
@@ -258,7 +396,6 @@ function getClosedPositions()
                     }
                   }
 
-                  // TODO: rr berekening werkend maken voor elk scenario, bijvoorbeeld entry en alleen tps of alleen sl of beide
                   // TODO: voor nu 1 entry, 1 sl en meerdere tps, maar iets verzinnen voor trades met meerdere entrie en sls
 
                   echo '<div class="card tCard shadow-lg text-white mb-3">' .
