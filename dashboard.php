71aa09aed550643dc5e5f0b02799ee6b9274d40e
illfamed <generalarmyhf@gmail.com>
2024-12-16 18:20:08 +0100

    layout changes

--- dashboard.php
+++ dashboard.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['loggedin']) !== true) {
                 </div>
                 <div class="modal-body">
                   <p class="mb-5">Selecteer het account dat je wil verwijderen</p>
-                  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="w-75 mx-auto">
                     <select class="form-select mb-5" name="selectedAccountToDelete">
                       <?php
                       // Assuming you have an array of accounts stored in $phemexAccounts
@@ -35,7 +35,6 @@ if (isset($_SESSION['loggedin']) !== true) {
                       }
                       ?>
                     </select>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Sluiten</button>
                     <input type="submit" value="Verwijderen" class="btn btn-danger">
                   </form>
                 </div>
@@ -44,7 +43,45 @@ if (isset($_SESSION['loggedin']) !== true) {
               </div>
             </div>
           </div>
-          <a href="addaccount.php" class="btn btn-outline-success d-inline float-end mx-2"><i class="bi bi-person-plus-fill"></i> Account toevoegen</a>
+          <button data-bs-toggle="modal" data-bs-target="#exampleModal2" class="btn btn-outline-success d-inline float-end mx-2"><i class="bi bi-person-plus-fill"></i> Account toevoegen</button>
+          <!-- Modal -->
+          <div data-bs-theme="dark" class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModal2Label" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModal2Label">Account toevoegen</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <form id="addAccForm" class="w-75 mx-auto" action="database/doaddaccount.php" method="POST">
+                    <div class="row">
+                      <label for="apikey" class="text-start">Phemex API Key</label>
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="bi bi-key"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="apikey" name="apikey" placeholder="Phemex API Key" required />
+                      </div>
+                      <label for="apisecret" class="text-start mt-4">Phemex API Secret</label>
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="bi bi-key"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="apisecret" name="apisecret" placeholder="Phemex API Seret" required />
+                      </div>
+                    </div>
+                    <div class="row">
+                      <button type="submit" class="mt-5 btn btn-success shadow-lg mx-auto">
+                        <i class="bi bi-plus-lg"></i> Toevoegen
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                </div>
+              </div>
+            </div>
+          </div>
         </h1>
         <div class="card-body panel-body p-5">
           <div class="row mb-5">
